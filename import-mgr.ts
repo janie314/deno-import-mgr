512a1a4975ff9e8223ad937f6a1fbfc663f590e0
12ball <pub@janie.page>
2024-01-06 14:31:53 -0600

    1704573113

--- import-mgr.ts
+++ import-mgr.ts
@@ -1,13 +1,18 @@
-import { join } from "https://deno.land/std@0.211.0/path/mod.ts";
+import { join } from "path";
 
-async function cache() {
+type DenoJson = { imports: { [key: string]: string } };
+
+async function read_deno_json(): Promise<DenoJson> {
   const cwd = Deno.cwd();
   const path = join(cwd, "deno.json");
-  const deno_json = await Deno.readTextFile(path).then((res) =>
-    JSON.parse(res)
-  );
-  return Promise.all(
-    Object.values(deno_json.imports).map(async (url) => {
+  return await Deno.readTextFile(path).then((res) => JSON.parse(res));
+}
+
+async function cache(
+  deno_json: DenoJson,
+): Promise<{ success: boolean; url: string }[]> {
+  return await Promise.all(
+    Object.values(deno_json.imports).map(async (url: string) => {
       const res = await new Deno.Command("deno", { args: ["info", url] })
         .output();
       return {
@@ -18,5 +23,6 @@ async function cache() {
   );
 }
 
-const res = await cache();
+const deno_json = await read_deno_json();
+const res = await cache(deno_json);
 console.table(res);
